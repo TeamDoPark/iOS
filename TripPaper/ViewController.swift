254455b650cb8a14ac3138d56775483d30a4a28c
SangWookPark <wkahdla12346@gmail.com>
2018-11-27 14:43:19 +0900

    CREATE project +++

---
+++ TripPaper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripPaper
+//
+//  Created by 박소정 on 27/11/2018.
+//  Copyright © 2018 sangwook park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
